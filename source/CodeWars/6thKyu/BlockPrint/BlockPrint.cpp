5b631517c79a46615c0cc44c5830564ba0003a64
AtennopVer <anton7185569@gmail.com>
2024-03-13 22:26:53 +0300

    Fixed last task

--- source/CodeWars/6thKyu/BlockPrint/BlockPrint.cpp
+++ source/CodeWars/6thKyu/BlockPrint/BlockPrint.cpp
@@ -1,34 +1,41 @@
-#include <string>
-#include <vector>
-#include <map>
+#include "Alphabet.h"
 
 namespace SixthKyu
 {
-    extern std::map<char, std::vector<std::string>> alpha;
-
     std::string BlockPrint(const std::string &input)
     {
+        if (input.length() == 0)
+            return "";
+
         int start = 0;
         int end = (int)input.size() - 1;
 
         while (input[start] == ' ' || input[end] == ' ')
         {
-            if (input[start] == ' ')
-                start++;
-
-            if (input[end] == ' ')
-                end--;
+            start += (input[start] == ' ' ? 1 : 0);
+            end += (input[end] == ' ' ? -1 : 0);
         }
 
+        if (start == (int)input.length() || end == -1)
+            return "";
+
         std::string result;
-        std::string message = std::string(&input[start], &input[end]);
+        std::string message = std::string(&input[start], &input[end] + 1);
 
         for (int i = 0; i < 7; i++)
         {
-            for (char letter : message)
-                result += alpha[(char)(letter > 'Z' ? letter : letter + 32)][i] + ' ';
+            for (char j : message)
+                result += alpha[(char)((j > 'Z' || j == ' ') ? j : j + ' ')][i] + ' ';
 
-            result += '\n';
+            for (int j = (int)result.length() - 1; j >= 0; j--)
+            {
+                if (result[j] != ' ')
+                    break;
+
+                result.erase(j);
+            }
+
+            result += (i != 6 ? "\n" : "");
         }
 
         return result;
